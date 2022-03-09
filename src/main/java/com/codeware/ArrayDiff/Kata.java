710ad49a2ae246c978ae7887230e822c548d95bd
Agata <agata.weltrowska@outlook.com>
2022-03-09 20:43:08 +0100

    next commit

--- src/main/java/com/codeware/ArrayDiff/Kata.java
+++ src/main/java/com/codeware/ArrayDiff/Kata.java
@@ -1,2 +1,48 @@
-package com.codeware.ArrayDiff;public class Kata {
+package com.codeware.ArrayDiff;
+
+import java.util.*;
+
+/**
+ * Your goal in this kata is to implement a difference function, which subtracts one list
+ * from another and returns the result.
+ * <p>
+ * It should remove all values from list a, which are present in list b keeping their order.
+ * <p>
+ * Kata.arrayDiff(new int[] {1, 2}, new int[] {1}) => new int[] {2}
+ * If a value is present in b, all of its occurrences must be removed from the other:
+ * <p>
+ * Kata.arrayDiff(new int[] {1, 2, 2, 2, 3}, new int[] {2}) => new int[] {1, 3}
+ */
+
+public class Kata {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(arrayDiff(new int[]{1, 2, 3}, new int[]{1, 2, 5, 6, 7, 8})));
+    }
+
+    public static int[] arrayDiff(int[] a, int[] b) {
+        List<Integer> result = new LinkedList<>();
+        if (b.length == 0) {
+            return a;
+        } else if (a.length == 0) {
+            return new int[0];
+        } else {
+            for (int i : a) {
+                boolean flag = true;
+                for (int n : b) {
+                    if (i == n) {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag) {
+                    result.add(i);
+                }
+            }
+            int[] r = new int[result.size()];
+            int i = 0;
+            for (Integer res : result) r[i++] = res;
+            return r;
+        }
+    }
+
 }
--- src/main/java/com/codeware/MaximumSubarraySum/Max.java
+++ src/main/java/com/codeware/MaximumSubarraySum/Max.java
@@ -1,2 +1,22 @@
-package com.codeware.MaximumSubarraySum;public class Max {
+package com.codeware.MaximumSubarraySum;
+
+/**
+ * The maximum sum subarray problem consists in finding the maximum sum of a contiguous subsequence
+ * in an array or list of integers:
+ * Max.sequence(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4});
+ * // should be 6: {4, -1, 2, 1}
+ * <p>
+ * Easy case is when the list is made up of only positive numbers and the maximum sum is the sum of the whole array.
+ * If the list is made up of only negative numbers, return 0 instead.
+ * <p>
+ * Empty list is considered to have zero greatest sum. Note that the empty list or array is also a valid sublist/subarray.
+ */
+
+public class Max {
+    public static int sequence(int[] arr) {
+        for (int a : arr) {
+            // TODO !!!!!!!!!!!!
+        }
+        return 4;
+    }
 }
